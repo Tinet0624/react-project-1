219bc64df5fa1920b3837baee2ecf370f03a6fe9
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2022-07-19 17:15:33 -0400

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://Tinet0624.github.io/react-project-1/",
+  base: "/react-project-1/",
   plugins: [react()]
 })
